37f5aa3ede6c6423b1886a916c67f4dc138fad9e
carbonsoda <carbonsoda@outlook.com>
2025-06-03 19:48:46 +0800

    clarify esbuild fs alias

--- src/utils/fs.ts
+++ src/utils/fs.ts
@@ -3,5 +3,6 @@ import Fs from "node:fs";
 import type IsoFs from "@isomorphic-git/lightning-fs";
 import { type PromiseFsClient } from "isomorphic-git";
 
+// lightning-fs would be alias for node:fs in web extension and Fs would be of type IsoFs
 export const fs: PromiseFsClient =
 	"prototype" in Fs ? new (Fs as unknown as typeof IsoFs)("fs") : Fs;
