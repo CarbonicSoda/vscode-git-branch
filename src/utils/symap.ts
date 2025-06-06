a6a34f54fa0fae387c185c2f927940450411473e
carbonsoda <carbonsoda@outlook.com>
2025-06-06 16:06:56 +0800

    some interesting notes on symap

--- src/utils/symap.ts
+++ src/utils/symap.ts
@@ -1,3 +1,5 @@
+// other methods like has()/delete() could be implemented but not needed
+// could be extended to n-keys easily if needed, refer to `key`
 export class SyMap<V> {
 	private map = new Map<string, V>();
 
@@ -10,6 +12,7 @@ export class SyMap<V> {
 	}
 }
 
+// could be used to normalize n-keys with .sort().map()
 const key = (key1: string, key2: string) => {
 	return key1 > key2
 		? `${encodeURIComponent(key1)}|${encodeURIComponent(key2)}`
